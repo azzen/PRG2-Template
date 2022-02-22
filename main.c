5c8363c25244bc2ae68729c7feab2ce7ee78ec44
azzen <6373020+azzen@users.noreply.github.com>
2022-02-22 14:41:39 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+-----------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      :
+ Date creation  :
+ Description    :
+ Remarques(s)   :
+ Compilateur    : Mingw-w64 gcc 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello PRG2!\n");
+    return EXIT_SUCCESS;
+}
